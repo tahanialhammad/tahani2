b0c94edad366f602a524504923cd3eb07455daab
Tahani <t.alhammad@linq5.nl>
2023-08-19 17:23:21 +0200

    add is admin midleware

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiteController;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
+// Site
+Route::controller(SiteController::class)->group(function () {
+    Route::get('/', 'index')->name('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+ // Auth users
+ Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', [UserController::class, 'users'])->name('dashboard');
+});
+
+// Auth admin
+Route::middleware('is_admin')->group(function () {
+    Route::get('/dashboard', [UserController::class, 'index'])->name('dashboard');
+});
 
 require __DIR__.'/auth.php';
