67d1c83336ac3194b4607110ba7d02952456f1a4
Tahani <t.alhammad@linq5.nl>
2023-08-25 11:43:15 +0200

    update

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,5 +1,30 @@
+
 @extends('admin.admin-template')
 
+
 @section('content')
-<h1>admin content </h1>
-@stop 
+{{-- @include('snippets.flash') --}}
+<div id="dashboard">
+    <div class="col-xl-9 page-title-v2 pt-4 ps-4">
+        <h3 class="fw-bold m-0">
+            Hallo {{ \Auth::user()->name }}
+        </h3>
+    </div>
+
+
+    <div class="row g-0">
+        <div class="col-xl-9 p-4">
+            @include('admin.dashboard.portlets.overview')
+            @include('admin.dashboard.portlets.invoices')
+        </div>
+
+
+        <div class="col-xl-3 px-lg-4 p-4">
+            {{-- @include('v2.user.dashboards.portlets.news')
+
+
+            @include('v2.user.dashboards.portlets.faq') --}}
+        </div>
+    </div>
+</div>
+@stop
---
+++ resources/views/admin/dashboard/portlets/invoices.blade.php
@@ -0,0 +1,7 @@
+
+<div>
+    <h1>Invoices</h1>
+</div>
+
+
+
