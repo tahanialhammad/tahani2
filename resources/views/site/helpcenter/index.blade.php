0abdd1998ab277f342563d5c032a781734b5b49a
Tahani <t.alhammad@linq5.nl>
2023-09-07 12:14:33 +0200

    update

--- resources/views/site/helpcenter/index.blade.php
+++ resources/views/site/helpcenter/index.blade.php
@@ -10,5 +10,25 @@
 @stop
 
 @section('content')
-content
+    <div class="helpcenter mx-auto mx-xl-5 px-xl-5">
+        <div class="py-4">
+            <h3 class="fw-bold text-center mb-4">Categories</h3>
+            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3 g-lg-5 bg">
+                @forelse($sections->sortBy('sort_order') as $section)
+                    <div class="col">
+                        <a href="">
+                            <div class="card h-100 text-center shadow">
+                                <div class="card-body py-5 ">
+                                    <icon-home class="icon-xxl icon-light mb-5"></icon-home>
+                                    <h4 class="card-title fw-bold">{{ $section->name }}</h4>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                @empty
+                    no fag items
+                @endforelse
+            </div>
+        </div>
+    </div>
 @stop
