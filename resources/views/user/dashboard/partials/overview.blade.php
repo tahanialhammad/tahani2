2f605bb4b7d505d53519d08d4e0273f312d6e7bb
tahanialhammad <tahani_alhammad@yahoo.com>
2024-02-03 23:11:07 +0100

    update user nav

--- resources/views/user/dashboard/partials/overview.blade.php
+++ resources/views/user/dashboard/partials/overview.blade.php
@@ -1,1 +1,30 @@
-overview
+<div class="mt-4 pt-2">
+   <h5 class="fw-bold mb-4">
+       {{ trans('menu.actions') }}
+   </h5>
+
+<div class="card-grouptt d-flex overflow-scroll scrollbar-toptt pb-4">
+
+  <div class="card p-4 rounded shadow me-4">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card p-4 rounded shadow me-4">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+  <div class="card p-4 rounded shadow me-4">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    </div>
+  </div>
+</div>
+</div>
