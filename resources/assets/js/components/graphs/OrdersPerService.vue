81574106ffe5b4f887da0a677a150744225ced72
tahanialhammad <tahani_alhammad@yahoo.com>
2024-03-11 23:13:21 +0100

    update

---
+++ resources/assets/js/components/graphs/OrdersPerService.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <canvas id="orderPerServiceChart"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js/auto';
+
+export default {
+  name: 'ChartComponent',
+  props: ['chartdata'],
+  data() {
+    return {
+      months: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+    }
+  },
+  mounted() {
+    this.renderChart();
+  },
+  methods: { 
+    renderChart() {
+      const ctx = document.getElementById('orderPerServiceChart').getContext('2d');
+      const myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+          labels: this.months,
+          datasets: [{
+            label: 'Customers',
+            data: this.chartdata,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    }
+  }
+};
+</script>
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -3,19 +3,19 @@
 @section('content')
 {{-- @include('snippets.flash') --}}
 <div id="dashboard">
-    <div class="col-xl-9 page-title pt-4 ps-4">
+    <div class="col-xl-9 page-title mb-4">
         <h3 class="fw-bold m-0">
             Hallo {{ \Auth::user()->name }}
         </h3>
     </div>
 
     <div class="row g-0">
-        <div class="col-xl-9 p-4">
+        <div class="col-xl-9 p-4tt">
             @include('admin.dashboard.partials.overview')
             @include('admin.dashboard.partials.invoices')
         </div>
 
-        <div class="col-xl-3 px-lg-4 p-4">
+        <div class="col-xl-3 px-lg-4 p-4tt">
             @include('admin.dashboard.partials.newestServices')
             @include('admin.dashboard.partials.newestOrders')
         </div>
