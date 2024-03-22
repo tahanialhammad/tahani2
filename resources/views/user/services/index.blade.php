61e82c8d520d561101ecde745c02e4a23938416d
tahanialhammad <tahani_alhammad@yahoo.com>
2024-03-22 17:35:45 +0100

    update

--- resources/views/user/services/index.blade.php
+++ resources/views/user/services/index.blade.php
@@ -1,6 +1,5 @@
 @extends('user.user-template')
 
-
 @section('content')
 <div>
     <div class="page-title d-flex justify-content-between align-items-center mb-4">
@@ -24,16 +23,15 @@
                     <div class="card-body">
                         <h5 class="card-title fw-bold text-center"> {{ $serviceItem->title }}</h5>
                         <p class="card-text"> {{ $serviceItem->body }}</p>
-                        <div>
-                        @foreach($servicesWithPackages as $serviceWithPackages)
-    <h2>{{ $serviceWithPackages['service']->name }}</h2>
-    <ul>
-        @foreach($serviceWithPackages['packages'] as $package)
-            <li>{{ $package->name }}</li>
-        @endforeach
-    </ul>
-@endforeach
-                        </div>
+                        @if(count($serviceItem->packages ))
+                        <h6>{{ trans ('packages.package-list')}}</h6>
+                        <ul>
+                            @foreach ( $serviceItem->packages as $package)
+                            <li> <img src="/images/packages/{{ $package->code }}.svg" class="w-30" alt="{{$package->code}}"></li>
+                            @endforeach
+                        </ul>
+                        @endif
+
                     </div>
                     <div class="card-footer">
                         <small class="text-muted">Last boeked 3 mins ago</small>
@@ -49,44 +47,6 @@
             </div>
             @endforelse
         </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        @forelse ($services as $serviceItem)
-        @php
-        /** @var \Carbon\Carbon $created */
-        $created = $serviceItem->created_at;
-        @endphp
-        <div class="bg-white mb-2 p-4 border border-info rounded d-flex">
-            <div>
-                icon
-            </div>
-            <div>
-
-            </div>
-        </div>
-        @empty
-
-        <div class="card mb-3 p-4">
-            <div class="card-body p-0 text-center">
-                <h5> {{ trans('services.no-services') }} </h5>
-                <icon-empty></icon-empty>
-            </div>
-        </div>
-        @endforelse
     </div>
 </div>
 @stop
