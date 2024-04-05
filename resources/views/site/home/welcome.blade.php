717768f96429e75678b8f70acf5c0094c3593133
tahanialhammad <tahani_alhammad@yahoo.com>
2024-04-05 22:42:11 +0200

    update

--- resources/views/site/home/welcome.blade.php
+++ resources/views/site/home/welcome.blade.php
@@ -1,14 +1,12 @@
 @extends('site.site-template')
 
 @section('header')
-@include('site.home.partials.hero2')
 @include('site.home.partials.hero')
-
 @stop
 
 @section('content')
     {{-- <example-component></example-component> --}}
     @include('site.home.partials.about')
     @include('site.home.partials.packages')
-    @include('site.home.partials.customers')
+    @include('site.home.partials.portfolio')
 @stop
